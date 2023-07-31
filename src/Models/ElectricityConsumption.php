8760b4a07717ee8705c901e3abfb2845d81eb300
Marcel Breuer <breuer.marcel@outlook.com>
2023-07-31 09:55:25 +0200

    create scope for current user

--- src/Models/ElectricityConsumption.php
+++ src/Models/ElectricityConsumption.php
@@ -2,6 +2,7 @@
 
 namespace Breuermarcel\PowerConsumption\Models;
 
+use Breuermarcel\PowerConsumption\Scopes\AuthUserScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,7 +15,17 @@ class ElectricityConsumption extends Model
      */
     protected $table = "bm_electricity_consumptions";
 
-    protected $fillable = ["value", "timestamp"];
+    protected $fillable = ["user_id", "value", "timestamp"];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new AuthUserScope);
+    }
 
     public function user()
     {
--- src/Models/WaterConsumption.php
+++ src/Models/WaterConsumption.php
@@ -2,6 +2,7 @@
 
 namespace Breuermarcel\PowerConsumption\Models;
 
+use Breuermarcel\PowerConsumption\Scopes\AuthUserScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,7 +15,17 @@ class WaterConsumption extends Model
      */
     protected $table = "bm_water_consumptions";
 
-    protected $fillable = ["value", "timestamp"];
+    protected $fillable = ["user_id", "value", "timestamp"];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new AuthUserScope);
+    }
 
     public function user()
     {
